d167b373170b5f931fd2f45af4f6ebb39bb02a12
Ramxcodes <ramkrishnaswarnkar99@gmail.com>
2025-07-29 19:56:49 +0530

    fixed umami api rewrite rule

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
       },
       {
         source: "/api/send",
-        destination: process.env.NEXT_PUBLIC_UMAMI_SRC || "",
+        destination: "https://analytics.notesbuddy.in/api/send",
       },
     ];
   },
